36efdc64a2653c88b8fff721fd78d180475a6394
Jae woo kim <ta3844@naver.com>
2022-07-30 13:03:46 +0900

    entitiManagerTest add

--- src/test/java/com/data/productdata/data/repository/ItineraryRepositoryTest.java
+++ src/test/java/com/data/productdata/data/repository/ItineraryRepositoryTest.java
@@ -1,6 +1,8 @@
 package com.data.productdata.data.repository;
 
 import com.data.productdata.data.entity.Itinerary;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +11,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.AttributeOverride;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+import javax.persistence.PersistenceContext;
 
 import java.lang.reflect.Array;
 import java.util.Arrays;
@@ -21,6 +26,8 @@ class ItineraryRepositoryTest {
         @Autowired
         ItineraryRepository itineraryRepository;
 
+        @PersistenceContext
+        EntityManager entityManager;
         @Test
         void test(){
            List<String> sitess = Arrays.asList("경북궁","청계천","명동","인사동");
@@ -48,6 +55,25 @@ class ItineraryRepositoryTest {
             Itinerary chageItinerary = itineraryRepository.findById(1L).get();
             System.out.println(chageItinerary.getSites().get(0));
             System.out.println(chageItinerary);
+
+
+        }
+        @BeforeEach
+        void saved(){
+            List<String> sitess = Arrays.asList("경북궁","청계천","명동","인사동");
+            Itinerary itinerary =new Itinerary("광화문 종로 인근","설명",sitess);
+            itineraryRepository.save(itinerary);
+        }
+        @DisplayName("List 변경 - EntitiyManager")
+        @Test
+        @Transactional
+        void test3(){
+
+
+            Itinerary savedItinerary = entityManager.find(Itinerary.class,1L);
+            List<String> changeList = Arrays.asList("궁구웅","구궁궁","하남시티","춘천시티");
+            savedItinerary.setSites(changeList);
+            entityManager.persist(savedItinerary);
 
 
         }
